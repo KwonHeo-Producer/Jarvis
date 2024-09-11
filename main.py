f6368f072d30b3564f948427d5fa5f7c373fec65
KwonHeo-Producer <kwonheojp@gmail.com>
2024-09-11 17:06:31 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, HTTPException
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from dotenv import load_dotenv
-from chain_service import initialize_chat_chain  # 랭체인 관련 기능 임포트
+from service.stock.stock_service import GoogleSheetsService
+from pydantic import BaseModel
+import os
 
 # 환경 변수 로드
 load_dotenv('env/data.env')
@@ -12,10 +14,23 @@ app = FastAPI()
 # 정적 파일 서빙
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
+# Google Sheets API 설정
+SPREADSHEET_ID = os.getenv("SPREADSHEET_ID")  # 환경 변수로 구글 시트 ID 가져오기
+SHEET_NAME = 'Stock'  # 시트 이름
+
+# Google Sheets 서비스 객체 생성
+sheets_service = GoogleSheetsService(
+    spreadsheet_id=SPREADSHEET_ID,
+    sheet_name=SHEET_NAME
+)
+
 @app.get("/", response_class=HTMLResponse)
 async def read_index():
     with open("index.html", "r", encoding="utf-8") as file:
         return file.read()
+
+class Message(BaseModel):
+    prompt: str
 
 @app.post("/process_message")
 async def process_message(request: Request):
@@ -23,9 +38,9 @@ async def process_message(request: Request):
     prompt = data.get("prompt")
 
     if prompt:
-        # 챗봇 대화 체인을 통해 응답 생성
-        chain = initialize_chat_chain()
-        response_content = chain.run(prompt)
-        return {"response": response_content}
-
+        try:
+            response = sheets_service.process_message(prompt)
+            return response
+        except Exception as e:
+            raise HTTPException(status_code=500, detail=str(e))
     return {"error": "No prompt provided"}
