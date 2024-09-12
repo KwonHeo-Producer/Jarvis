787545efcb6a8df412bd6c53715f3b210218b29a
KwonHeo-Producer <kwonheojp@gmail.com>
2024-09-12 10:53:49 +0900

    Add files via upload

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
 from fastapi import FastAPI, Request, HTTPException
-from fastapi.responses import JSONResponse
+from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from dotenv import load_dotenv
 from service.stock.stock_service import GoogleSheetsService
 from pydantic import BaseModel
 import os
-import json
-import requests
 
 # 환경 변수 로드
 load_dotenv('env/data.env')
@@ -26,10 +24,6 @@ sheets_service = GoogleSheetsService(
     sheet_name=SHEET_NAME
 )
 
-# Gemini API 설정
-GEMINI_API_URL = os.getenv("GEMINI_API_URL")  # Gemini API URL
-GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")  # Gemini API Key
-
 @app.get("/", response_class=HTMLResponse)
 async def read_index():
     with open("index.html", "r", encoding="utf-8") as file:
@@ -45,30 +39,8 @@ async def process_message(request: Request):
 
     if prompt:
         try:
-            # 특정 패턴이 포함된 경우에만 Google Sheets 코드 실행
-            if "의 현재 주가는?" in prompt:
-                response = sheets_service.process_message(prompt)
-                return response
-            
-            # Gemini API로 대화 처리
-            gemini_response = process_with_gemini(prompt)
-            return gemini_response
-
+            response = sheets_service.process_message(prompt)
+            return response
         except Exception as e:
             raise HTTPException(status_code=500, detail=str(e))
     return {"error": "No prompt provided"}
-
-def process_with_gemini(prompt: str) -> dict:
-    headers = {
-        "Authorization": f"Bearer {GEMINI_API_KEY}",
-        "Content-Type": "application/json"
-    }
-    payload = {
-        "prompt": prompt
-    }
-    response = requests.post(GEMINI_API_URL, headers=headers, json=payload)
-    
-    if response.status_code == 200:
-        return response.json()
-    else:
-        raise HTTPException(status_code=response.status_code, detail="Error from Gemini API")
