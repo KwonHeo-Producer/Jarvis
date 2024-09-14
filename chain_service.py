15504a85cdc2722c800e658010414b690bfe42d7
KwonHeo-Producer <kwonheojp@gmail.com>
2024-09-14 12:59:39 +0900

    Update chain_service.py

--- chain_service.py
+++ chain_service.py
@@ -1,10 +1,9 @@
-# chain_service.py
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain.chains import ConversationChain
 from langchain.memory import ConversationBufferMemory
 
 def initialize_chat_chain():
     llm = ChatGoogleGenerativeAI(model="gemini-1.5-flash", temperature=1.0)
-    memory = ConversationBufferMemory(return_messages=True, k=5)
+    memory = ConversationBufferMemory(return_messages=True, k=None)  # k=None으로 무제한 설정
     chain = ConversationChain(llm=llm, memory=memory)
     return chain
