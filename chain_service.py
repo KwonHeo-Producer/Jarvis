3c5ee26b4f58b43e848e43ff9dd0e8cffd05a117
KwonHeo-Producer <kwonheojp@gmail.com>
2024-09-15 13:44:21 +0900

    Update chain_service.py

--- chain_service.py
+++ chain_service.py
@@ -4,7 +4,7 @@ from langchain.chains import ConversationChain
 from langchain.memory import ConversationBufferMemory
 
 def initialize_chat_chain():
-    llm = ChatGoogleGenerativeAI(model="gemini-1.5-flash", temperature=1.0)
-    memory = ConversationBufferMemory(return_messages=True, k=5)
+    llm = ChatGoogleGenerativeAI(model="gemini-1.5-flash", temperature=0.5)
+    memory = ConversationBufferMemory(return_messages=True, k=100)
     chain = ConversationChain(llm=llm, memory=memory)
     return chain
