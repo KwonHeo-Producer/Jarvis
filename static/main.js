2d4d4239cec5f6ff2b62de04503e0094b45b9e6a
KwonHeo-Producer <kwonheojp@gmail.com>
2024-09-21 13:38:27 +0900

    Update main.js

--- static/main.js
+++ static/main.js
@@ -1,11 +1,8 @@
 // main.js
 
 // Load chat.js
+import initChat from './static/js/chat.js';
+
 document.addEventListener('DOMContentLoaded', () => {
-    const script = document.createElement('script');
-    script.src = '/static/js/chat.js';
-    script.onload = () => {
-        // chat.js 로드 후 실행할 코드
-    };
-    document.body.appendChild(script);
+    initChat();
 });
