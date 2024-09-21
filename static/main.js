e49e4bf2eaf426e80566b5352638de4be8532ece
KwonHeo-Producer <kwonheojp@gmail.com>
2024-09-21 11:45:42 +0900

    Update main.js

--- static/main.js
+++ static/main.js
@@ -2,5 +2,5 @@
 
 // Load chat.js
 const script = document.createElement('script');
-script.src = 'path/to/chat.js'; // Update this path to the actual location of chat.js
+script.src = 'static/chat.js'; // Update this path to the actual location of chat.js
 document.head.appendChild(script);
